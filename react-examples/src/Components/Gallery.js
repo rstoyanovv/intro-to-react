a7968d5736e3755459096cade37e491dc6c6ae63
rosko03 <rostistoqnov@gmail.com>
2023-09-08 23:38:18 +0300

    Update

--- react-examples/src/Components/Gallery.js
+++ react-examples/src/Components/Gallery.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import car from '../assets/car_model2.png';
+import car1 from '../assets/car_model1.png';
+import car2 from '../assets/car_model2.png';
+import car3 from '../assets/car_model3.png';
+import car4 from '../assets/car_model4.png';
+import car5 from '../assets/car_model5.png';
+import car6 from '../assets/car_model6.png';
 
-function Profile() {
+function PictureOfCar({image}) {
     return (
-        <img src={car} alt="Katherine Johnson" />
+        <img src={image} alt="Car" />
     );
 }
 
@@ -14,13 +19,15 @@ function Header() {
 }
 
 export default function Gallery() {
+    const carImages = [car1, car2, car3, car4, car5, car6];
+
     return (
         <section style={{ marginLeft: 30 }}>
             <Header />
             <div>
-                <Profile />
-                <Profile />
-                <Profile />
+                {carImages.map((image, index) => (
+                    <PictureOfCar key={index} image={image} />
+                ))}
             </div>
         </section>
     );
