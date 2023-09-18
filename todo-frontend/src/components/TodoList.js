d0218a2023c91d89b1110e17b074e3fb4d4f8172
rosko03 <rostistoqnov@gmail.com>
2023-09-18 11:32:18 +0300

    Add css to file

--- todo-frontend/src/components/TodoList.js
+++ todo-frontend/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../styles/style.css';
 
 export default function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -27,20 +28,23 @@ export default function TodoList() {
     };
 
     return (
-        <>
-            <h1> To-Do List </h1>
-            <input
-                type="text"
-                placeholder='Add new task'
-                value={newTask}
-                onChange={(e) => setNewTask(e.target.value)}
-            />
-            <button onClick={addTodo}> Add </button>
-            <ul>
+        <div className="todo-container">
+            <h1 className="todo-header">To-Do List</h1>
+            <div className="input-container">
+                <input
+                    type="text"
+                    placeholder='Add new task'
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                    className="task-input"
+                />
+                <button onClick={addTodo} className="add-button">Add</button>
+            </div>
+            <ul className="todo-list">
                 {todos.map((todo) => (
-                    <li key={todo.id}> {todo.task} </li>
+                    <li key={todo.id} className="todo-item">{todo.task}</li>
                 ))}
             </ul>
-        </>
+        </div>
     );
 }
