12ad8967bc40a7e88f248ae4a092a8035fb579ad
rstoyanovv <rostistoqnov@gmail.com>
2023-09-28 22:28:13 +0300

    Upgrade navbar

--- todo-frontend/src/components/Navbar.js
+++ todo-frontend/src/components/Navbar.js
@@ -1,9 +1,26 @@
 export default function Navbar() {
+    const styleOfATag = {
+        display: 'block',
+        textDecoration: 'none',
+        padding: '20px 20px',
+    }
+
+    const styleOfButton = {
+        border: 'white',
+        borderRadius: '30px',
+    }
+
     return (
         <nav>
-            <h3>Simple To-Do application</h3>
-            <button><a href='/api/create-task'> Create a new task </a></button>
-            <button><a href='/api/get-tasks'> View all tasks </a></button>
+            <a href='/api/create-task'>
+                <h3>Simple To-Do application</h3>
+            </a>
+            <button style={styleOfButton}>
+                <a style={styleOfATag} href='/api/create-task'> Create a new task </a>
+            </button>
+            <button style={styleOfButton}>
+                <a style={styleOfATag} href='/api/get-tasks'> View all tasks </a>
+            </button>
         </nav>
     );
 }
