1a1977bfb71597ef4011371869f4c1e99191ae4c
rstoyanovv <rostistoqnov@gmail.com>
2023-10-03 22:50:22 +0300

    Finish navbar

--- todo-frontend/src/components/Navbar.js
+++ todo-frontend/src/components/Navbar.js
@@ -1,23 +1,21 @@
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">Home</a>
+            <div className="container-fluid ">
+                <a className="navbar-brand ms-2" href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-check-all me-2"
+                        viewBox="0 0 16 16" style={{marginBottom: '0.2rem'}}><path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z" />
+                    </svg>Task Mate</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/api/create-task">Create a new task</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/api/get-tasks">View all tasks</a>
-                        </li>
+                    <ul className="navbar-nav ">
+                        <a href="/api/create-task" className="btn btn-secondary mt-1 mb-1 me-4">Create new task</a>
+                        <a href="/api/get-tasks" className="btn btn-secondary mt-1 mb-1 me-4">View all tasks</a>
                     </ul>
                 </div>
             </div>
         </nav>
-
     );
 }
