afb21b353d562d1c151ff6910554057febfe774d
rstoyanovv <rostistoqnov@gmail.com>
2023-10-03 12:49:25 +0300

    Adding Bootstrap

--- todo-frontend/src/components/Navbar.js
+++ todo-frontend/src/components/Navbar.js
@@ -1,34 +1,23 @@
 export default function Navbar() {
-    const styleOfATag = {
-        display: 'block',
-        textDecoration: 'none',
-        padding: '20px 20px',
-    }
-
-    const styleOfButton = {
-        border: 'white',
-        borderRadius: '30px',
-    }
-
-    const styleOfNavButtons = {
-        position: 'absolute',
-        right: '0',
-    }
-
     return (
-        <nav>
-            <a href='/'>
-                <h3>Simple To-Do application</h3>
-            </a>
-            <div className="nav-button-section" style={styleOfNavButtons}>
-                <button style={styleOfButton}>
-                    <a style={styleOfATag} href='/api/create-task'> Create a new task </a>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Home</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <button style={styleOfButton}>
-                    <a style={styleOfATag} href='/api/get-tasks'> View all tasks </a>
-                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/api/create-task">Create a new task</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/api/get-tasks">View all tasks</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-
         </nav>
+
     );
 }
