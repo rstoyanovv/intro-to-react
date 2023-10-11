9866fb2901da7f48b1c9404d990e30242c3f3725
rstoyanovv <115493787+rstoyanovv@users.noreply.github.com>
2023-10-11 10:34:10 +0300

    Delete todo-app directory

--- todo-app/src/databaseInitials/todos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE todos (
-    id serial PRIMARY KEY,
-    title VARCHAR NOT NULL,
-    task VARCHAR NOT NULL,
-    is_completed BOOLEAN NOT NULL,
-    date_of_creating DATE DEFAULT CURRENT_DATE
-);
